fd25527d8e90ba4f5006b858b11ced9baf9da93d
AshAritra <86512807+AshAritra@users.noreply.github.com>
2022-11-13 13:26:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,45 +14,8 @@ const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/
 const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./lib/myfunc')
 const figlet = require('figlet')
 const { color } = require('./lib/color')
-///healthchecker for render. if u want u can disable it.
-const healthcheck = require('./routes/healthchecker')
 
 const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
-///////////////////////////////////////////
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const session = require('express-session');
-dotenv.config();
-const passport = require("passport");
-const { loginCheck } = require("./auth/passport");
-loginCheck(passport);
-// Mongo DB conncetion
-const database = process.env.MONGOLAB_URI;
-mongoose.connect(database, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then(() => console.log("e don connect"))
-  .catch((err) => console.log(err));
-app.set("view engine", "ejs");
-//BodyParsing
-app.use(express.urlencoded({ extended: false }));
-app.use(session({
-    secret:'oneboy',
-    saveUninitialized: true,
-    resave: true
-  }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-//Routes
-app.use("/", require("./routes/login"));
-app.use('/healthcheck', require('./routes/healthchecker'));
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, console.log("Server has started at port " + PORT));
-
-///////////////////////////////////////////////////////////////////////
-
 
 
 async function startMiku() {
