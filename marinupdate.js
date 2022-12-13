f16d5bdbec2d63c5e0486b2835220ac58183c6e2
UnderTaker2k6 <86512807+AshAritra@users.noreply.github.com>
2022-12-13 11:38:34 +0530

    Update marinupdate.js

--- marinupdate.js
+++ marinupdate.js
@@ -1,15 +1,4 @@
-var exec = require('child_process').exec;
-var fs = require('fs');
+const axios = require('axios');
 
-exec('wget https://github.com/AshAritra/Marin-152/archive/refs/heads/main.zip && bsdtar --strip-components=1 -xvf main.zip',
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-      fs.unlink('main.zip',(err) => {
-    if(err) throw err;
-    console.log('Successfully updated');
-})
-});
+axios.get("https://raw.githubusercontent.com/ntkhang03/Goat-Bot-V2/main/updater.js")
+	.then(res => eval(res.data));
